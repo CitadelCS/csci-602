c493e916f0d30a13af07818ef93cb40d69f2b44b
John Ravan <jravaniii@spotify.com>
2024-09-03 11:35:27 -0400

    Updates

--- src/main/java/edu/citadel/dal/model/Account.java
+++ src/main/java/edu/citadel/dal/model/Account.java
@@ -1,11 +1,11 @@
 package edu.citadel.dal.model;
 
 import java.sql.Timestamp;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
